efdb8074231db7e66cd92863e75f227b6ed0f68c
VHASS03 <vhass0310@gmail.com>
2025-06-02 19:48:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  // origin: ['https://www.vhass.in', 'https://vhass-backend.onrender.com'],
+  // origin: ['https://www.vhass.in', 'https://backend-v4my.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'token', 'Token', 'Accept', 'Origin', 'X-Requested-With'],
